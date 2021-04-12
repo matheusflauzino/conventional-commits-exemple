8de389d2ee6cb336be32904d0a36cc0a882ac3e0
Matheus Flauzino <matheus.flauzino@gerencianet.com.br>
2021-04-11 21:23:52 -0300

    test: criado o metodo toString para a classe

--- src/test/entities/person.entity.spec.ts
+++ src/test/entities/person.entity.spec.ts
@@ -1,15 +1,28 @@
 import Person from '../../entities/person.entity'
 
 describe('Person Entity', () => {
-  test('ensures that entity was created', () => {
-    const entity = new Person({
+  let entity: Person
+
+  beforeEach(() => {
+    entity = new Person({
       name: 'Matheus',
       age: 30,
-      sex: 'male',
+      sex: 'male'
+    })
     })
 
+  test('ensures that entity was created', () => {
     expect(entity.name).toBe('Matheus')
     expect(entity.age).toBe(30)
     expect(entity.sex).toBe('male')
   })
+
+  test('ensures that when calling toString, the object returns the values', () => {
+    expect(entity.toString()).toEqual({
+      name: 'Matheus',
+      age: 30,
+      sex: 'male'
+    })
+  })
+
 })
